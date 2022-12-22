ad0c8a040629c54bc51b972dc3594b0835749556
JaeJinByun <byunjin11@naver.com>
2022-12-22 19:06:00 +0900

    [UPDATE] 주문자:주문 생성, 주문 리스트 조회, 리뷰 작성,리스트 조회 기능 추가

--- go-mvc-project/services/orderer.service.go
+++ go-mvc-project/services/orderer.service.go
@@ -4,4 +4,7 @@ import "wba/go-mvc-procjet/model"
 
 type OrdererService interface {
 	CreateOrder(*model.Order) error
+	GetAllOrder(sort string) ([]model.Menu, error)
+	GetAllReiview(menuName string) (float64, []model.Review, error)
+	CreateReview(*model.Review, string) error
 }
--- go-mvc-project/services/orderer.service.impl.go
+++ go-mvc-project/services/orderer.service.impl.go
@@ -2,24 +2,118 @@ package services
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 	"wba/go-mvc-procjet/model"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type OrdererServiceImpl struct {
-	ordercollection *mongo.Collection
-	ctx             context.Context
+	orderCollection  *mongo.Collection
+	reviewCollection *mongo.Collection
+	menuCollection   *mongo.Collection
+	ctx              context.Context
 }
 
-func NewOrdererService(orderercollection *mongo.Collection, ctx context.Context) OrdererService {
+func NewOrdererService(mc *mongo.Collection, oc *mongo.Collection, rc *mongo.Collection, ctx context.Context) (OrdererService, error) {
 	return &OrdererServiceImpl{
-		ordercollection: orderercollection,
-		ctx:             ctx,
-	}
+		menuCollection:   mc,
+		orderCollection:  oc,
+		reviewCollection: rc,
+		ctx:              ctx,
+	}, nil
 }
 
+/* 주문 생성 */
 func (o *OrdererServiceImpl) CreateOrder(order *model.Order) error {
-	_, err := o.ordercollection.InsertOne(o.ctx, order)
+	order.CreatedAt = time.Now()
+	_, err := o.orderCollection.InsertOne(o.ctx, order)
+	return err
+}
+
+/* 모든 메뉴 리스트 조회 */
+func (o *OrdererServiceImpl) GetAllOrder(sort string) ([]model.Menu, error) {
+	//sort = [recommend, grade, reorder, createdat]
+
+	filter := bson.M{"isdelete": false}
+	opts := options.Find().SetSort(bson.D{{Key: sort, Value: -1}})
+
+	var menulist []model.Menu
+
+	if corsur, err := o.menuCollection.Find(o.ctx, filter, opts); err != nil {
+		panic(err)
+	} else if err := corsur.All(o.ctx, &menulist); err != nil {
+		panic(err)
+	}
+	return menulist, nil
+}
+
+/* 특정 메뉴에 대한 리뷰들 조회 */
+func (o *OrdererServiceImpl) GetAllReiview(menuName string) (float64, []model.Review, error) {
+
+	filter := bson.M{"menuname": menuName, "isdelete": false}
+	var reivewlist []model.Review
+	if corsur, err := o.reviewCollection.Find(o.ctx, filter); err != nil {
+		panic(err)
+	} else if err := corsur.All(o.ctx, &reivewlist); err != nil {
+		panic(err)
+	}
+
+	/* 그룹 스테이지 생성 - 메뉴이름 기준 */
+	groupStage := bson.D{
+		{Key: "$group", Value: bson.D{
+			{Key: "_id", Value: "$menuname"},
+			{Key: "avg_grade", Value: bson.D{{Key: "$avg", Value: "$grade"}}},
+			{Key: "type_total", Value: bson.D{{Key: "$sum", Value: 1}}},
+		}}}
+
+	/* 집계함수에 그룹스테이지 조건 대입 */
+	cursor, err := o.reviewCollection.Aggregate(o.ctx, mongo.Pipeline{groupStage})
+	if err != nil {
+		panic(err)
+	}
+
+	/* 결과 표시 */
+	var results []bson.M
+	if err = cursor.All(o.ctx, &results); err != nil {
+		panic(err)
+	}
+	fmt.Println(results[0]["avg_grade"])
+
+	avgGrade := results[0]["avg_grade"].(float64)
+	return avgGrade, reivewlist, nil
+}
+
+/* 리뷰 작성 */
+func (o *OrdererServiceImpl) CreateReview(review *model.Review, id string) error {
+	var order *model.Order
+
+	objId, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": objId}
+	o.orderCollection.FindOne(o.ctx, filter).Decode(&order)
+
+	/* 예외처리 조건 : 주문 상태가 5 (배달완료) 가 아니거나 이미 리뷰가 존재하는 주문이라면 + */
+	if order.IsExistReview || order.Status != 5 {
+		return errors.New("리뷰를 작성할 수 없습니다")
+	}
+
+	review.CreatedAt = time.Now()
+	review.IsDetele = false
+	if _, err := o.reviewCollection.InsertOne(o.ctx, review); err != nil {
+		return err
+	}
+	/* 해당 주문에 대해서 리뷰 작성완료 업데이트 */
+	query := bson.M{
+		"$set": bson.M{
+			"isexistreview": true,
+		},
+	}
+	_, err := o.orderCollection.UpdateByID(o.ctx, objId, query)
+
 	return err
 }
