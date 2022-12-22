785590052bca198be2bb01b80decf25d50060c7d
JaeJinByun <byunjin11@naver.com>
2022-12-22 19:07:22 +0900

    [UPDATE] 주문자 컨트롤러 주문생성, 메뉴리스트조회, 리뷰작성, 리뷰조회 추가

--- go-mvc-project/controller/orderer.controller.go
+++ go-mvc-project/controller/orderer.controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 	"wba/go-mvc-procjet/model"
 	"wba/go-mvc-procjet/services"
@@ -19,18 +18,61 @@ func NewOrdererController(ordererservice services.OrdererService) (OrdererContro
 	}, nil
 }
 
-// 주문자 api 컨트롤러 작성
+/* 주문 생성 */
 func (oc *OrdererController) CreateOrder(ctx *gin.Context) {
 	var order model.Order
-	fmt.Println(ctx.Request)
+	/* BINDING */
 	if err := ctx.ShouldBindJSON(&order); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
+	/* CREATE */
 	err := oc.OrdererService.CreateOrder(&order)
 	if err != nil {
 		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
 		return
 	}
+	/* RETURN */
 	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+/* 메뉴 리스트 조회 */
+func (oc *OrdererController) GetAllOrder(ctx *gin.Context) {
+	menulist, err := oc.OrdererService.GetAllOrder(ctx.Param("sort"))
+	/* GET */
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, menulist)
+}
+
+/* 리뷰 작성 */
+func (oc *OrdererController) CreateReview(ctx *gin.Context) {
+	var review model.Review
+	id := ctx.Param("objectId")
+	/* BINDING */
+	if err := ctx.ShouldBindJSON(&review); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	/* CREATE */
+	err := oc.OrdererService.CreateReview(&review, id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	/* RETURN */
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+/* 메뉴별 평점 및 리뷰 조회 */
+func (oc *OrdererController) GetAllReiview(ctx *gin.Context) {
+	avgGrade, reviewlist, err := oc.OrdererService.GetAllReiview(ctx.Param("menuname"))
+	/* GET */
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"avgGrade": avgGrade, "reviewlist": reviewlist})
 }
