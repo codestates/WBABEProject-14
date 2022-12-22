5a0df6c48c9b3faadb6d3d723ba0fcc46123864f
JaeJinByun <byunjin11@naver.com>
2022-12-22 19:04:58 +0900

    [UPDATE] 메뉴 생성,수정,삭제,추천 태깅 기능 추가

--- go-mvc-project/services/taker.service.go
+++ go-mvc-project/services/taker.service.go
@@ -4,4 +4,7 @@ import "wba/go-mvc-procjet/model"
 
 type TakerService interface {
 	CreateMenu(*model.Menu) error
+	UpdateMenu(*model.Menu) error
+	DeleteMenu(*model.Menu) error
+	UpdateMenuRecommend(*model.Menu) error
 }
--- go-mvc-project/services/taker.service.impl.go
+++ go-mvc-project/services/taker.service.impl.go
@@ -2,24 +2,79 @@ package services
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 	"wba/go-mvc-procjet/model"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type TakerServiceImpl struct {
-	takercollection *mongo.Collection
-	ctx             context.Context
+	menuCollection *mongo.Collection
+	ctx            context.Context
 }
 
-func NewTakerService(takercollection *mongo.Collection, ctx context.Context) TakerService {
+func NewTakerService(mc *mongo.Collection, ctx context.Context) (TakerService, error) {
 	return &TakerServiceImpl{
-		takercollection: takercollection,
-		ctx:             ctx,
-	}
+		menuCollection: mc,
+		ctx:            ctx,
+	}, nil
 }
 
-func (o *TakerServiceImpl) CreateMenu(order *model.Menu) error {
-	_, err := o.takercollection.InsertOne(o.ctx, order)
+/* 메뉴 등록 */
+func (o *TakerServiceImpl) CreateMenu(menu *model.Menu) error {
+	/* 메뉴이름 중복 검사 */
+	var isExistMenu model.Menu
+	filter := bson.D{bson.E{Key: "menuname", Value: menu.MenuName}}
+	o.menuCollection.FindOne(o.ctx, filter).Decode(&isExistMenu)
+	if len(isExistMenu.MenuName) > 0 {
+		fmt.Println(isExistMenu.MenuName)
+		return errors.New("동일한 메뉴 이름이 존재합니다")
+	}
+	menu.CreatedAt = time.Now()
+	_, err := o.menuCollection.InsertOne(o.ctx, menu)
+	return err
+}
+
+/* 메뉴 수정 */
+func (o *TakerServiceImpl) UpdateMenu(menu *model.Menu) error {
+	filter := bson.M{"menuname": menu.MenuName}
+	query := bson.M{
+		"$set": bson.M{
+			"price":       menu.Price,
+			"origin":      menu.Origin,
+			"orderstatus": menu.OrderStatus,
+		},
+	}
+	_, err := o.menuCollection.UpdateOne(o.ctx, filter, query)
+
+	return err
+}
+
+/* 메뉴 삭제 */
+func (o *TakerServiceImpl) DeleteMenu(menu *model.Menu) error {
+	filter := bson.M{"menuname": menu.MenuName}
+	query := bson.M{
+		"$set": bson.M{
+			"isdelete": true,
+		},
+	}
+	_, err := o.menuCollection.UpdateOne(o.ctx, filter, query)
+	return err
+}
+
+/* 금일 추천 메뉴 변경 */
+func (o *TakerServiceImpl) UpdateMenuRecommend(menu *model.Menu) error {
+	var result model.Menu
+	filter := bson.M{"menuname": menu.MenuName}
+	o.menuCollection.FindOne(o.ctx, filter).Decode(&result)
+	query := bson.M{
+		"$set": bson.M{
+			"recommend": !result.Recommend,
+		},
+	}
+	_, err := o.menuCollection.UpdateOne(o.ctx, filter, query)
 	return err
 }
