9d9094ee8d506ec6c362036b546679f215f62c8b
JaeJinByun <byunjin11@naver.com>
2022-12-25 08:04:32 +0900

    [UPDATE] 컨트롤러 swagger 작성 완료

--- go-mvc-project/controller/orderer.controller.go
+++ go-mvc-project/controller/orderer.controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 	"wba/go-mvc-procjet/model"
 	"wba/go-mvc-procjet/services"
 
@@ -18,61 +19,154 @@ func NewOrdererController(ordererservice services.OrdererService) (OrdererContro
 	}, nil
 }
 
-/* 주문 생성 */
+// @Summary 💡API 주문 생성 요청
+// @Description 새로운 주문을 생성합니다. json 포맷으로 요청합니다. 반환값은 주문번호 입니다.
+// @name CreateOrder
+// @Param  request body model.ApiMenuCreateRequest true "주문 생성 요청"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/orderer/order [POST]
+// @Success 201 {object} model.ApiMenuCreateResponse "주문 생성 성공"
+// @Failure 400 {object} model.Failure "주문 생성 실패"
 func (oc *OrdererController) CreateOrder(ctx *gin.Context) {
 	var order model.Order
 	/* BINDING */
 	if err := ctx.ShouldBindJSON(&order); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadRequest, model.Failure{Message: err.Error()})
 		return
 	}
 	/* CREATE */
-	err := oc.OrdererService.CreateOrder(&order)
+	orderNumber, err := oc.OrdererService.CreateOrder(&order)
 	if err != nil {
-		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadGateway, model.Failure{Message: err.Error()})
 		return
 	}
 	/* RETURN */
-	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+	ctx.JSON(http.StatusCreated, gin.H{"주문번호": orderNumber})
 }
 
-/* 메뉴 리스트 조회 */
-func (oc *OrdererController) GetAllOrder(ctx *gin.Context) {
-	menulist, err := oc.OrdererService.GetAllOrder(ctx.Param("sort"))
+// @Summary 💡API 메뉴 리스트 조회
+// @Description 메뉴 리스트를 조회합니다. Path 로 정렬할 데이터를 받습니다. [recommend, grade, reorder, createdat]
+// @name GetAllMenu
+// @Param sort path string true "Sorting Parameter"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/orderer/menu/{sort} [GET]
+// @Success 200 {object} model.ApiMenuListResponse "메뉴 리스트 조회 성공"
+// @Failure 400 {object} model.Failure "조회 실패"
+func (oc *OrdererController) GetAllMenu(ctx *gin.Context) {
+	menulist, err := oc.OrdererService.GetAllMenu(ctx.Param("sort"))
 	/* GET */
 	if err != nil {
-		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadGateway, model.Failure{Message: err.Error()})
 		return
 	}
 	ctx.JSON(http.StatusOK, menulist)
 }
 
-/* 리뷰 작성 */
+// @Summary 💡API 리뷰 작성 요청
+// @Description 완료된 주문에 리뷰를 작성합니다. 해당 주문의 ID를 받습니다. 성공/실패 여부를 반환합니다.
+// @name CreateReview
+// @Param  orderID path string true "Review ID"
+// @Param  request body model.ApiCreateReviewRequest true "리뷰 생성 요청 Body"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/orderer/review/{orderID} [POST]
+// @Success 201 {object} model.Success "리뷰 작성 성공"
+// @Failure 400 {object} model.Failure "리뷰 작성 실패"
 func (oc *OrdererController) CreateReview(ctx *gin.Context) {
 	var review model.Review
-	id := ctx.Param("objectId")
+	id := ctx.Param("orderID")
 	/* BINDING */
 	if err := ctx.ShouldBindJSON(&review); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadRequest, model.Failure{Message: err.Error()})
 		return
 	}
 	/* CREATE */
 	err := oc.OrdererService.CreateReview(&review, id)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadRequest, model.Failure{Message: err.Error()})
 		return
 	}
 	/* RETURN */
-	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+	ctx.JSON(http.StatusOK, model.Success{Message: "success"})
 }
 
-/* 메뉴별 평점 및 리뷰 조회 */
-func (oc *OrdererController) GetAllReiview(ctx *gin.Context) {
+// @Summary 💡API  메뉴 상세 조회
+// @Description 각 메뉴별 상세 정보를 조회합니다. 메뉴 이름을 받습니다. 해당 메뉴의 평점과 리뷰 리스트를 반환합니다.
+// @name GetMenuDetail
+// @Param  menuname path string true "Menu Name"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/orderer/detailMenu/{menuname} [GET]
+// @Success 200 {object} model.ApiGetMenuDetailResponse "메뉴 상세 조회 성공"
+// @Failure 400 {object} model.Failure "메뉴 상세 조회 실패"
+func (oc *OrdererController) GetMenuDetail(ctx *gin.Context) {
 	avgGrade, reviewlist, err := oc.OrdererService.GetAllReiview(ctx.Param("menuname"))
 	/* GET */
 	if err != nil {
-		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		ctx.JSON(http.StatusBadGateway, model.Failure{Message: err.Error()})
 		return
 	}
 	ctx.JSON(http.StatusOK, gin.H{"avgGrade": avgGrade, "reviewlist": reviewlist})
+}
+
+// @Summary 💡API 주문 변경 ( 메뉴 변경 및 추가 )
+// @Description 주문한 메뉴를 변경 또는 추가 합니다. 주문ID, 추가 또는 변경의 Flag,  메뉴를 Body 로 받습니다. flag 가 0 일땐 메뉴 추가이며 배달중일 경우 신규주문으로 처리, 배달중이 아닐경우 성공을 반환합니다. flag 가 1일땐 메뉴 변경이며 조리중,배달중 일경우 실패, 아닐경우 성공을 반환합니다.
+// @name UpdateOrder
+// @Param  orderId path string true "Order Id"
+// @Param  flag path string true "AddMenu : 0, UpdateMenu : 1"
+// @Param  request body model.ApiUpdateOrderRequest true "변경 또는 추가할 메뉴"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/orderer/order/{orderId}/{flag} [PATCH]
+// @Success 201 {object} model.ApiUpdateOrderResponse "메뉴 추가 실패시 신규주문처리"
+// @Success 200 {object} model.Success "메뉴 추가 성공시"
+// @Failure 400 {object} model.Failure "메뉴 변경 실패시"
+func (oc *OrdererController) UpdateOrder(ctx *gin.Context) {
+	var order *model.Order
+	id := ctx.Param("orderId")
+	status, err := strconv.Atoi(ctx.Param("flag"))
+	/* BINDING */
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, model.Failure{Message: err.Error()})
+		return
+	}
+	if err := ctx.ShouldBind(&order); err != nil {
+		ctx.JSON(http.StatusBadRequest, model.Failure{Message: err.Error()})
+		return
+	}
+	/* UPDATE */
+	orderNumber, updateError := oc.OrdererService.UpdateOrder(id, status, order.MenuName)
+	if updateError != nil {
+		ctx.JSON(http.StatusBadRequest, model.Failure{Message: updateError.Error()})
+		return
+	}
+
+	/* RESPONSE */
+	if orderNumber == -1 {
+		/* 메뉴 추가 성공 */
+		ctx.JSON(http.StatusCreated, model.Success{Message: "success"})
+		return
+	}
+	/* 메뉴 추가 실패 , 신규 주문으로 처리, 신규 주문번호 반환 */
+	ctx.JSON(http.StatusCreated, gin.H{"message": "해당 주문은 배달중입니다. 신규주문으로 처리되었습니다.", "주문 번호": orderNumber})
+}
+
+// @Summary 💡API 주문 내역 조회
+// @Description 주문 내역을 조회 합니다. 현재 주문과 주문이 완료된 이전 주문들을 반환합니다.
+// @name GetOrders
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/orderer/orders [GET]
+// @Success 200 {object} model.ApiGetOrdersResponse "주문 내역 조회 성공"
+// @Failure 400 {object} model.Failure "주문 내역 조회 실패"
+func (oc *OrdererController) GetOrders(ctx *gin.Context) {
+	currents, pastorders, err := oc.OrdererService.GetOrders()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, model.Failure{Message: err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"currentOrders": currents, "pastOrders": pastorders})
+
 }
--- go-mvc-project/controller/taker.controller.go
+++ go-mvc-project/controller/taker.controller.go
@@ -18,7 +18,15 @@ func NewTakerController(takerservice services.TakerService) (TakerController, er
 	}, nil
 }
 
-/* 메뉴 등록 */
+// @Summary 💡API 신규 메뉴 등록
+// @Description 신규 메뉴를 생성합니다.
+// @Param  request body model.ApiCreateMenuRequest true "메뉴 등록 요청 Body"
+// @name CreateMenu
+// @Accept   json
+// @Produce  json
+// @Router /api/v01/taker/menu [POST]
+// @Success 201 {object} model.Success "메뉴 생성 성공"
+// @Failure 400 {object} model.Failure "메뉴 생성 실패(동일 메뉴이름이 존재)"
 func (oc *TakerController) CreateMenu(ctx *gin.Context) {
 	var menu model.Menu
 	/* BINDING */
@@ -35,16 +43,26 @@ func (oc *TakerController) CreateMenu(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, gin.H{"message": "success"})
 }
 
-/* 메뉴 수정 */
+// @Summary 💡API 기존 메뉴 수정
+// @Description 기존 메뉴를 수정합니다.
+// @Param menuname path string true "Menu Name"
+// @Parma menuname
+// @Param request body model.ApiUpdateMenuRequest true "메뉴 수정 요청 Body"
+// @Accept  json
+// @Produce json
+// @Router /api/v01/taker/menu/{menuname} [PUT]
+// @Success 200 {object} model.Success "메뉴 수정 성공"
+// @Failure 400 {object} model.Failure "메뉴 수정 실패"
 func (oc *TakerController) UpdateMenu(ctx *gin.Context) {
 	var menu model.Menu
+	menuname := ctx.Param("menuname")
 	/* BINDING */
 	if err := ctx.ShouldBindJSON(&menu); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 	/* UPDATE */
-	if err := oc.TakerService.UpdateMenu(&menu); err != nil {
+	if err := oc.TakerService.UpdateMenu(menuname, &menu); err != nil {
 		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
 		return
 	}
@@ -52,7 +70,15 @@ func (oc *TakerController) UpdateMenu(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
-/* 메뉴 삭제 */
+// @Summary 💡API 기존 메뉴 삭제
+// @Description 기존 메뉴 삭제 플래그를 변경(false -> true)합니다.
+// @name DeleteMenu
+// @Param  request body model.ApiUpdateOrderRequest true "메뉴 삭제 요청 Body"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/taker/menu [DELETE]
+// @Success 200 {object} model.Success "메뉴 삭제 성공"
+// @Failure 400 {object} model.Failure "메뉴 삭제 실패"
 func (oc *TakerController) DeleteMenu(ctx *gin.Context) {
 	var menu model.Menu
 	/* BINDING */
@@ -69,17 +95,70 @@ func (oc *TakerController) DeleteMenu(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
-/* 금일 추천 메뉴 변경 */
+// @Summary 💡API 금일 추천 메뉴 변경
+// @Description 추천 메뉴를 변경합니다. 요청한 메뉴가  금일 추천 메뉴라면 추천하지 않음으로, 추천 메뉴가 아니라면 추천 메뉴로 설정합니다. 응답으로 추천 메뉴들을 반환합니다.
+// @name UpdateMenuRecommend
+// @Param  request body model.ApiUpdateOrderRequest true "금일 추천 메뉴 변경 요청 Body"
+// @Accept   json
+// @Produce  json
+// @Router /api/v01/taker/menu [PATCH]
+// @Success 200 {object} model.ApiUpdateMenuRecommend "추천 메뉴 변경 성공"
+// @Failure 400 {object} model.Failure "추천 메뉴 변경 실패"
 func (oc *TakerController) UpdateMenuRecommend(ctx *gin.Context) {
-	var menu model.Menu
+	var menu *model.Menu
 	/* BINDING */
 	if err := ctx.ShouldBindJSON(&menu); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 	/* UPDATE */
-	if err := oc.TakerService.UpdateMenuRecommend(&menu); err != nil {
+	menus, err := oc.TakerService.UpdateMenuRecommend(menu)
+	if err != nil {
 		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	/* RESPONSE */
+	ctx.JSON(http.StatusOK, menus)
+}
+
+// @Summary 💡API 주문 리스트 조회
+// @Description 주문 리스트를 조회합니다. 주문 상태가 접수되기전 상태인 주문들만 조회합니다 (주문이 막 들어온 상태).
+// @name GetOrderList
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/taker/orders [GET]
+// @Success 200 {object} model.ApiGetOrderListResponse "주문 조회 성공"
+// @Failure 400 {object} model.Failure "주문 조회 실패"
+func (oc *TakerController) GetOrderList(ctx *gin.Context) {
+	/* RESPONSE */
+	if orderList, err := oc.TakerService.GetOrderList(); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+	} else {
+		ctx.JSON(http.StatusOK, orderList)
+	}
+}
+
+// @Summary 💡API  각 메뉴별 주문 상태 변경
+// @Description 각 메뉴별 주문들의 상태를 다음 단계로 변경합니다. 메뉴이름 과 현재 상태를 요청합니다. 해당 메뉴의 0(접수중)인 주문들을 1(접수완료) 상태로 업데이트 합니다.
+// @name UpdateOrderStatus
+// @Param  request body model.ApiUpdateOrderStatusRequest true "메뉴별 상태 업데이트 요청 Body"
+// @Param  menuname path string true "Menu Name"
+// @Accept  json
+// @Produce  json
+// @Router /api/v01/taker/orders/{menuname} [PATCH]
+// @Success 200 {object} model.Success "메뉴별 주문 상태 저장 성공"
+// @Failure 400 {object} model.Failure "저장 실패"
+func (oc *TakerController) UpdateOrderStatus(ctx *gin.Context) {
+	var order *model.Order
+	orderId := ctx.Param("menuname")
+	/* BINDING */
+	if err := ctx.ShouldBindJSON(&order); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	/* UPDATE */
+	if err := oc.TakerService.UpdateOrderStatus(orderId, order.Status); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 	/* RESPONSE */
