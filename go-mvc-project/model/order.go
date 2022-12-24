e19115ef2caf6a88d4bd90b30d54af35b01f6c86
JaeJinByun <byunjin11@naver.com>
2022-12-24 23:12:44 +0900

    [DELETE] validate 검증 삭제

--- go-mvc-project/model/order.go
+++ go-mvc-project/model/order.go
@@ -6,11 +6,11 @@ import (
 
 // 주문
 type Order struct {
-	MenuName      string    `json:"menuname" bson:"menuname" validate:"required"`
-	Phone         string    `json:"phone" bson:"phone" validate:"required"`
-	Address       string    `json:"address" bson:"address" validate:"required"`
-	CreatedAt     time.Time `json:"createdat" bson:"createdat" validate:"required"`
-	IsDelete      bool      `json:"isdelete" bson:"isdelete" validate:"required"`
-	IsExistReview bool      `json:"isexistreview" bson:"isexistreview" validate:"required"`
-	Status        int       `json:"status" bson:"status" validate:"required"`
+	MenuName      string    `json:"menuname" bson:"menuname"`
+	Phone         string    `json:"phone" bson:"phone"`
+	Address       string    `json:"address" bson:"address"`
+	CreatedAt     time.Time `json:"createdat" bson:"createdat"`
+	IsDelete      bool      `json:"isdelete" bson:"isdelete"`
+	IsExistReview bool      `json:"isexistreview" bson:"isexistreview"`
+	Status        int       `json:"status" bson:"status"`
 }
