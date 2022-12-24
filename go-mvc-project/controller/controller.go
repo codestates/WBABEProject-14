eaa0f57f98b7035f5ca3ac31dcbd93932852cc03
JaeJinByun <byunjin11@naver.com>
2022-12-25 08:04:15 +0900

    [DELETE] 사용하지 않는 함수 삭제

--- go-mvc-project/controller/controller.go
+++ go-mvc-project/controller/controller.go
@@ -19,17 +19,3 @@ func NewCTL(rep *model.Model) (*Controller, error) {
 func (p *Controller) RespOK(c *gin.Context, resp interface{}) {
 	c.JSON(http.StatusOK, resp)
 }
-
-// GetOK godoc
-// @Summary call GetOK, return ok by json.
-// @Description api test를 위한 기능.
-// @name GetOK
-// @Accept  json
-// @Produce  json
-// @Param name path string true "User name"
-// @Router /acc/v01/ok [get]
-// @Success 200 {object} Controller
-func (p *Controller) GetOK(c *gin.Context) {
-	c.JSON(200, gin.H{"msg": "ok"})
-	return
-}
