bd302a8d54df3d60602e0704e04289952b870c67
JaeJinByun <byunjin11@naver.com>
2022-12-31 23:05:34 +0900

    [UPDATE] 주문 상태를 Enum 타입으로 정의

--- go-mvc-project/model/order.go
+++ go-mvc-project/model/order.go
@@ -6,6 +6,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Status int
+
 /* 주문 */
 type Order struct {
 	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty" example:"63a73a1c8d989838729bc114"`
@@ -14,13 +16,21 @@ type Order struct {
 	Address       string             `json:"address" bson:"address" example:"서울시 강남구 위메이드동 1212-202 101호"`
 	CreatedAt     time.Time          `json:"createdat" bson:"createdat" example:"2022-12-24T16:17:12.793+00:00"`
 	IsDelete      bool               `json:"isdelete" bson:"isdelete" example:"false"`
-	IsExistReview bool               `json:"isexistreview" bson:"isexistreview" example:"false"`\
+	IsExistReview bool               `json:"isexistreview" bson:"isexistreview" example:"false"`
 	/*
-	Status의 경우 상수를 통해 입력받는다면 사용하는 곳에서 코드를 읽기에 쉬워집니다. 
-	관련 키워드 : Enum, Const
+		Status의 경우 상수를 통해 입력받는다면 사용하는 곳에서 코드를 읽기에 쉬워집니다.
+		관련 키워드 : Enum, Const
 	*/
-	Status        int                `json:"status" bson:"status" example:"0"`
-	OrderNumber   int                `json:"ordernumber" bson:"ordernumber" example:"11"`
+	Status      Status `json:"status" bson:"status" example:"0"`
+	OrderNumber int    `json:"ordernumber" bson:"ordernumber" example:"11"`
 }
 
 /* 상태 */
+const (
+	Ordering Status = iota
+	OrderCancel
+	AddOrder
+	Cooking
+	Delivering
+	Complete
+)
--- go-mvc-project/services/taker.service.go
+++ go-mvc-project/services/taker.service.go
@@ -8,5 +8,5 @@ type TakerService interface {
 	DeleteMenu(*model.Menu) error                           //피주문자 - 메뉴 삭제
 	UpdateMenuRecommend(*model.Menu) ([]*model.Menu, error) //피주문자 - 오늘의 추천 메뉴 수정
 	GetOrderList() ([]*model.Order, error)                  //피주문자 - 현재 주문내역 리스트 조회
-	UpdateOrderStatus(string, int) error                    //피주문자 - 메뉴별 주문 요청 상태변경 (접수, 접수취소, 조리중, 추가주문, 배달중)
+	UpdateOrderStatus(string, model.Status) error           //피주문자 - 메뉴별 주문 요청 상태변경 (접수, 접수취소, 조리중, 추가주문, 배달중)
 }
