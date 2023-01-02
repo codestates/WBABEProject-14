e756f9bf36a119a4ca6f77dc9faa1c7a7ef9a0ad
JaeJinByun <byunjin11@naver.com>
2023-01-03 01:14:09 +0900

    [UPDATE]  메뉴 구조체 가게번호 필드와 UpdatedAt 필드 추가

--- go-mvc-project/model/menu.go
+++ go-mvc-project/model/menu.go
@@ -15,6 +15,7 @@ import (
 // 메뉴
 type Menu struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty" example:"63a73a1c8d989838729bc114"`
+	StoreNumber int                `json:"storenumber" bson:"storenumber" example:"001"`
 	MenuName    string             `json:"menuname" bson:"menuname" example:"떡볶이"`
 	OrderStatus bool               `json:"orderstatus" bson:"orderstatus" example:"true"`
 	LimitOrder  int                `json:"limitorder" bson:"limitorder" example:"100"`
@@ -25,9 +26,10 @@ type Menu struct {
 	Reorder     int                `json:"reorder" bson:"reorder" example:"30"`
 	Recommend   bool               `json:"recommend" bson:"recommend" example:"true"`
 	/*
-	일반적으로 created_at, updated_at은 한 세트로 이루어집니다.
-	그 이유는 추후 값 변경의 히스토리 추적을 위해서 updated_at이 용이합니다. 또한, 가장 최근에 변경된 순으로 정렬을 하는 경우에도 용이하구요.
+		일반적으로 created_at, updated_at은 한 세트로 이루어집니다.
+		그 이유는 추후 값 변경의 히스토리 추적을 위해서 updated_at이 용이합니다. 또한, 가장 최근에 변경된 순으로 정렬을 하는 경우에도 용이하구요.
 	*/
-	CreatedAt   time.Time          `json:"createdat" bson:"createdat" example:"2022-12-24T16:17:12.793+00:00"`
-	Grade       float64            `json:"grade" bson:"grade" example:"3.5"`
+	CreatedAt time.Time `json:"createdat" bson:"createdat" example:"2022-12-24T16:17:12.793+00:00"`
+	UpdatedAt time.Time `json:"updatedat" bson:"updatedat" example:"2022-12-25T16:17:12.793+00:00"`
+	Grade     float64   `json:"grade" bson:"grade" example:"3.5"`
 }
